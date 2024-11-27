49afaaed0b631b97c7c49f1761939545c5055b83
panhariths <panharith.sun@gmail.com>
2024-11-27 15:13:48 +0700

    release version 2.0.27

--- EkycID.framework/Headers/EkycID-Swift.h
+++ EkycID.framework/Headers/EkycID-Swift.h
@@ -350,6 +350,7 @@ SWIFT_CLASS("_TtC6EkycID21LivenessDetectionView")
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)coder SWIFT_UNAVAILABLE;
 @end
 
+
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
